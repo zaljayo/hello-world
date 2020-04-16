98a4c5a6497a346f15ca5053d68f879a329b189a
jaehanjeong <jhjeong@nzero.kr>
2020-04-16 16:37:33 +0900

    gitignore test1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,7 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World hahahah");
-		System.out.println("Hello World hihihihi");
-		System.out.println("Hello World hehehe");
+		System.out.println("Hello World");
+	
 	}
 }
